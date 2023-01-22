d543d92798cd8146bd08fed0720f2a44c252b039
Girish Hegde <girish.dhc@gmail.com>
2023-01-22 09:04:00 +0530

    wikitext prepare.py added

---
+++ GPT/datasets/wikitext/prepare.py
@@ -0,0 +1,28 @@
+"""
+Refs:
+    https://huggingface.co/datasets/wikitext
+"""
+
+
+import pickle
+from pathlib import Path
+
+import numpy as np
+from datasets import load_dataset
+
+
+__author__ = "__Girish_Hegde__"
+
+
+split = 'train'
+outdir = '../../data/'
+
+wikitext = load_dataset("wikitext", 'wikitext-2-v1')
+dataset = wikitext[split]
+print(f'dataset info: {dataset}')
+dataset = dataset['text']
+
+outdir = Path(outdir)
+outdir.mkdir(exist_ok=True, parents=True)
+with (outdir/f'wikitext_{split}.pkl').open('wb') as fp:
+    pickle.dump(dataset, fp)
