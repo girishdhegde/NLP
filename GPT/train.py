8ff86c773cb31ce4d56cddb18ed58120ac6ff416
Girish Hegde <girish.dhc@gmail.com>
2023-01-24 21:39:47 +0530

    training loop without eval added

--- GPT/train.py
+++ GPT/train.py
@@ -27,6 +27,7 @@ DROPOUT = 0.0  # for pretraining 0 is good, for finetuning try 0.1+
 # logging
 LOGDIR = Path('./data/runs')
 LOAD = LOGDIR/'best.pt'  # or None
+PRINT_INTERVAL = 100
 # dataset
 DATASET = './GPT/data/wikitext_train.pkl'
 CACHE_DIR = './GPT/data/cache/wikitext_train'
@@ -36,7 +37,6 @@ EVAL_CACHE_DIR = './GPT/data/cache/wikitext_train'
 BATCH_SIZE = 16
 GRAD_ACC_STEPS = 1  # used to simulate larger batch sizes
 MAX_ITERS = 600000  # total number of training iterations
-# EPOCHS = 100
 EVAL_INTERVAL = 2000
 EVAL_ITERS = 200
 EVAL_ONLY = False  # if True, script exits right after the first eval
@@ -77,7 +77,9 @@ else:
 trainset = PretrainSet(trainset, CONTEXT)
 evalset = PretrainSet(evalset, CONTEXT)
 trainloader = DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=False)
+trainloader.len = MAX_ITERS*GRAD_ACC_STEPS
 evalloader = DataLoader(evalset, batch_size=BATCH_SIZE, shuffle=False)
+evalloader.len = EVAL_ITERS
 print(f'Total training samples = {len(trainset)}')
 
 # =============================================================
@@ -119,10 +121,53 @@ def get_lr(iter):
 # # =============================================================
 # # Training loop - forward, backward, optimize
 # # =============================================================
-# net.train()
-# # determine the learning rate for this iteration
-# if DECAY_LR:
-#     lr = get_lr(itr)
-#     for param_group in optimizer.param_groups:
-#         param_group['lr'] = lr
+trainloss, evalloss, log_trainloss = 0, 0, 0
+trainloader, evalloader = iter(trainloader), iter(evalloader)
+net.train()
+for itr in range(itr, MAX_ITERS):
+    if DECAY_LR:
+        lr = get_lr(itr)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+    optimizer.zero_grad()
 
+    for step in range(GRAD_ACC_STEPS):
+        inp, tar = next(trainloader)
+        inp, tar = inp.to(DEVICE), tar.to(DEVICE)
+
+        logits = net(inp)
+        loss = criterion(logits.reshape(-1, tokenizer.n_vocab), tar.reshape(-1))
+        loss.backward()
+
+        loss_ = loss.item()
+        trainloss += loss_
+        log_trainloss += loss_
+
+    if GRADIENT_CLIP is not None:
+        nn.utils.clip_grad_norm_(net.parameters(), GRADIENT_CLIP)
+    optimizer.step()
+
+    if itr%PRINT_INTERVAL == 0:
+        log_data = f"iteration: {itr}/{MAX_ITERS}, \ttrain loss: {log_trainloss/PRINT_INTERVAL}"
+        print(log_data)
+        log_trainloss = 0
+
+    # if itr%EVAL_INTERVAL == 0:
+    #     trainloss = trainloss/EVAL_INTERVAL
+    #     if trainloss < best:
+    #         best = trainloss
+    #         save_checkpoint(
+    #             in_int2tk, out_int2tk, '<S>', '<E>', '<P>', '<U>',
+    #             net, epoch, trainloss, best, LOGDIR/f'best.pt',
+    #         )
+    #     save_checkpoint(
+    #         in_int2tk, out_int2tk, '<S>', '<E>', '<P>', '<U>',
+    #         net, epoch, trainloss, best, LOGDIR/f'checkpoint.pt',
+    #     )
+    #     write_pred(inp[0], pred[0], in_int2tk, out_int2tk, LOGDIR/'predictions.txt', label=f'epoch = {epoch}')
+    #     logfile = LOGDIR/'log.txt'
+    #     log_data = f"epoch: {epoch}/{EPOCHS}, \tloss: {trainloss},\t best_loss: {best}"
+    #     print(f'{"-"*100}\n{log_data}\n{"-"*100}')
+    #     with open(logfile, 'a' if logfile.is_file() else 'w') as fp:
+    #         fp.write(log_data + '\n')
+    #     trainloss = 0
