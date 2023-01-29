82d20615e8755ad1f0145fa4dea647a1f6d2168a
Girish Hegde <girish.dhc@gmail.com>
2023-01-29 11:42:14 +0530

    seed bug(rerunnig on same data aftr resume) fix.

--- GPT/utils.py
+++ GPT/utils.py
@@ -10,8 +10,8 @@ __author__ = "__Girish_Hegde__"
 
 
 def set_seed(seed):
-    random.seed(seed)
-    np.random.seed(seed)
+    # random.seed(seed)
+    # np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
